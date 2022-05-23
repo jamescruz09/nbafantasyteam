976a5089e037d0a8a4d836ae366cebce92f34c6b
jamescruz09 <elishajamescruz09@gmail.com>
2022-05-23 18:17:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+"use strict"
+
+//point guard
+
+document.querySelector('.curry').addEventListener('click', function () {
+    document.querySelector('.first-pic').src = 'https://i.ibb.co/XDFPCQ5/stephcurry.png';
+
+    document.querySelector('.first-video').src = 'https://www.youtube.com/embed/WWLUzEoturE?controls=0&autoplay=1&mute=1&playlist=WWLUzEoturE&loop=1';
+
+});
+
+document.querySelector('.morant').addEventListener('click', function () {
+    document.querySelector('.first-pic').src = 'https://i.ibb.co/Y8LfTHg/morant.png';
+
+    document.querySelector('.first-video').src = 'https://www.youtube.com/embed/8On9YlIp8RQ?controls=0&autoplay=1&mute=1&playlist=8On9YlIp8RQ&loop=1';
+
+});
+
+document.querySelector('.paul').addEventListener('click', function () {
+    document.querySelector('.first-pic').src = 'https://i.ibb.co/RhBgQzW/paul.png';
+
+    document.querySelector('.first-video').src = 'https://www.youtube.com/embed/5RvfbbzA_h8?controls=0&autoplay=1&mute=1&playlist=5RvfbbzA_h8&loop=1';
+
+});
+
+//shooting guard
+
+document.querySelector('.booker').addEventListener('click', function () {
+    document.querySelector('.second-pic').src = 'https://i.ibb.co/FnHGZyY/booker.png';
+
+    document.querySelector('.second-video').src = 'https://www.youtube.com/embed/5zfVi_MVdDU?controls=0&autoplay=1&mute=1&playlist=5zfVi_MVdDU&loop=1';
+
+});
+
+document.querySelector('.mitchell').addEventListener('click', function () {
+    document.querySelector('.second-pic').src = 'https://i.ibb.co/TmRgfZh/mitchell.png';
+
+    document.querySelector('.second-video').src = 'https://www.youtube.com/embed/QEOYgUgqFvI?controls=0&autoplay=1&mute=1&playlist=QEOYgUgqFvI&loop=1';
+
+});
+
+document.querySelector('.thompson').addEventListener('click', function () {
+    document.querySelector('.second-pic').src = 'https://i.ibb.co/nkwnh4Q/thompson.png';
+
+    document.querySelector('.second-video').src = 'https://www.youtube.com/embed/n0ELjEYdQ8E?controls=0&autoplay=1&mute=1&playlist=n0ELjEYdQ8E&loop=1';
+
+});
+
+// small forward
+
+document.querySelector('.james').addEventListener('click', function () {
+    document.querySelector('.third-pic').src = 'https://i.ibb.co/YXvgzk4/86404-16521064289423-1920-removebg-preview.png';
+
+    document.querySelector('.third-video').src = 'https://www.youtube.com/embed/0cu1EGvJjGk?controls=0&autoplay=1&mute=1&playlist=0cu1EGvJjGk&loop=1';
+
+});
+
+document.querySelector('.leonard').addEventListener('click', function () {
+    document.querySelector('.third-pic').src = 'https://i.ibb.co/9c55Jpn/leonard.png';
+
+    document.querySelector('.third-video').src = 'https://www.youtube.com/embed/6y7EXrsG058?controls=0&autoplay=1&mute=1&playlist=6y7EXrsG058&loop=1';
+
+});
+
+document.querySelector('.butler').addEventListener('click', function () {
+    document.querySelector('.third-pic').src = 'https://i.ibb.co/K5Y7mPy/butler.png';
+
+    document.querySelector('.third-video').src = 'https://www.youtube.com/embed/f0BQ8jh8U8g?controls=0&autoplay=1&mute=1&playlist=f0BQ8jh8U8g&loop=1';
+
+});
+
+//power forward
+
+document.querySelector('.ante').addEventListener('click', function () {
+    document.querySelector('.fourth-pic').src = 'https://i.ibb.co/6swT9PY/ante.png';
+
+    document.querySelector('.fourth-video').src = 'https://www.youtube.com/embed/0USzJs9agOk?controls=0&autoplay=1&mute=1&playlist=0USzJs9agOk&loop=1';
+
+});
+
+document.querySelector('.durant').addEventListener('click', function () {
+    document.querySelector('.fourth-pic').src = 'https://i.ibb.co/6Xp4PC9/durant.png';
+
+    document.querySelector('.fourth-video').src = 'https://www.youtube.com/embed/eu5lOIgWO3c?controls=0&autoplay=1&mute=1&playlist=eu5lOIgWO3c&loop=1';
+
+});
+
+document.querySelector('.tatum').addEventListener('click', function () {
+    document.querySelector('.fourth-pic').src = 'https://i.ibb.co/6RNBDt2/tatum.png';
+
+    document.querySelector('.fourth-video').src = 'https://www.youtube.com/embed/6sDKjWt9pM0?controls=0&autoplay=1&mute=1&playlist=6sDKjWt9pM0&loop=1';
+
+});
+
+// center
+
+document.querySelector('.embiid').addEventListener('click', function () {
+    document.querySelector('.fifth-pic').src = 'https://i.ibb.co/drCmf4r/embiid.png';
+
+    document.querySelector('.fifth-video').src = 'https://www.youtube.com/embed/njh0u0thW94?controls=0&autoplay=1&mute=1&playlist=njh0u0thW94&loop=1';
+
+});
+
+document.querySelector('.jokic').addEventListener('click', function () {
+    document.querySelector('.fifth-pic').src = 'https://i.ibb.co/2tBp6GS/jokic.png';
+
+    document.querySelector('.fifth-video').src = 'https://www.youtube.com/embed/d3EKuIc7wT8?controls=0&autoplay=1&mute=1&playlist=d3EKuIc7wT8&loop=1';
+
+});
+
+document.querySelector('.gobert').addEventListener('click', function () {
+    document.querySelector('.fifth-pic').src = 'https://i.ibb.co/ySXqdD9/gobert.png';
+
+    document.querySelector('.fifth-video').src = 'https://www.youtube.com/embed/W5jjtdfZuLM?controls=0&autoplay=1&mute=1&playlist=W5jjtdfZuLM&loop=1';
+
+});
+
